65821b686d240a4ee026a5cc60af1ae0ba969897
Anton Beza <anton.beza@gmail.com>
2018-06-09 22:26:29 -0400

    Added success messages for beating achievements

--- src/client/views/PuzzlePackListView.js
+++ src/client/views/PuzzlePackListView.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { PlayerPropType, PuzzlePackPropType } from './prop-types';
 
 import PuzzlePackView from './PuzzlePackView';
+import AchievementIcon from './AchievementIcon';
 
 export default class PuzzlePackListView extends Component {
 
@@ -25,6 +26,65 @@ export default class PuzzlePackListView extends Component {
     return buttons;
   }
 
+  renderAchievements () {
+    const { player, puzzlePacks } = this.props;
+    const completedAllPuzzles = puzzlePacks.every(pack => 
+      pack.puzzles.every(p => player.getSolutionByPuzzleId(p.id).records.completed.timestamp > 0)
+    );
+    const perfectStepsAllPuzzles = puzzlePacks.every(pack => 
+      pack.puzzles.every(p => player.getSolutionByPuzzleId(p.id).records.steps.perfect)
+    );
+    const perfectPatternsAllPuzzles = puzzlePacks.every(pack => 
+      pack.puzzles.every(p => player.getSolutionByPuzzleId(p.id).records.patterns.perfect)
+    );
+    if (perfectStepsAllPuzzles && perfectPatternsAllPuzzles) {
+      return (
+        <div style={{ marginTop: 32, textAlign: 'center' }}>
+          <AchievementIcon type="completed" styling="solid" />
+          <AchievementIcon type="steps" styling="solid" />
+          <AchievementIcon type="patterns" styling="solid" />
+          <br/><br/>
+          ACHIEVED: EVERYTHING - YOU'RE A GENIUS!
+        </div>
+      );
+    }
+    const records = [];
+    if (completedAllPuzzles) {
+      records.push(
+        <div key="completed" style={{ marginTop: 32, textAlign: 'center' }}>
+          <AchievementIcon type="completed" styling="solid" />
+          <AchievementIcon type="completed" styling="solid" />
+          <AchievementIcon type="completed" styling="solid" />
+          <br/><br/>
+          ACHIEVED: COMPLETED every puzzle!
+        </div>
+      );
+    }
+    if (perfectStepsAllPuzzles) {
+      records.push(
+        <div key="steps" style={{ marginTop: 32, textAlign: 'center' }}>
+          <AchievementIcon type="steps" styling="solid" />
+          <AchievementIcon type="steps" styling="solid" />
+          <AchievementIcon type="steps" styling="solid" />
+          <br/><br/>
+          ACHIEVED: PERFECT STEPS every puzzle!
+        </div>
+      );
+    }
+    if (perfectPatternsAllPuzzles) {
+      records.push(
+        <div key="patterns" style={{ marginTop: 32, textAlign: 'center' }}>
+          <AchievementIcon type="patterns" styling="solid" />
+          <AchievementIcon type="patterns" styling="solid" />
+          <AchievementIcon type="patterns" styling="solid" />
+          <br/><br/>
+          ACHIEVED: PERFECT PATTERNS every puzzle!
+        </div>
+      );
+    }
+    return records;
+  }
+
   render () {
     const {
       player,
@@ -34,10 +94,11 @@ export default class PuzzlePackListView extends Component {
       navigateToEditPuzzle,
       isEditable,
     } = this.props;
-    const lockedPuzzlePackIndex = puzzlePacks.findIndex(pack => {
+    const lockedPuzzlePackIndex = (puzzlePacks.findIndex(pack => {
       const lastPuzle = pack.puzzles[pack.puzzles.length - 1];
       return !player.getSolutionByPuzzleId(lastPuzle.id).records.completed.timestamp
-    }) + 1;
+    }) + 1) || puzzlePacks.length;
+    
     const unlockedPacks = isEditable ?
       puzzlePacks :
       puzzlePacks.filter((pack, index) => index < lockedPuzzlePackIndex);
@@ -58,6 +119,8 @@ export default class PuzzlePackListView extends Component {
             isEditable={isEditable}
           />
         )}
+        {this.renderAchievements()}
+        <div style={{ marginBottom: 200 }}>&nbsp;</div>
       </div>
     );
   }
